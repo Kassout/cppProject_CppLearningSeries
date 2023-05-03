0aab657bbbf34b3cf1a7a468ad71acd3124482b2
Maxime Héliot <maxime.heliotpro@gmail.com>
2023-05-03 15:33:50 +0200

    Add Classes in C++

--- HelloWorld/src/Main.cpp
+++ HelloWorld/src/Main.cpp
@@ -2,26 +2,24 @@
 
 #define LOG(x) std::cout << x << std::endl
 
-void Increment(int* value) // use pointer to get the memory address of the variable passed by as parameter so we can modify it.
+class Player
 {
-	(*value)++; // dereference the value so that we can actually write to that memory instead of modifying the pointer itself
-}
+public:
+	int x, y;
+	int speed;
 
-void Increment(int& value) // pass by references, exactly the same thing as above but clearer
-{
-	value++;
-}
+	void Move(int xa, int ya)
+	{
+		x += xa * speed;
+		y += ya * speed;
+	}
+};
 
 int main()
 {
-	int a = 5;
-	int& ref = a; // ref is a, Reference is just creating an allias of a to be used as if it was a.
-	ref = 2;
-
-	LOG(a);
-
-	Increment(a); // use ampersand to get the memory address reference of that variable
-	LOG(a);
+	Player player;
+	player.x = 5;
+	player.Move(1, -1);
 
 	std::cin.get();
 }
