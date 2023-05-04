2d2a357b2f16b78fa2a59b295caae925d9e5964f
Maxime Héliot <maxime.heliotpro@gmail.com>
2023-05-04 14:43:33 +0200

    Add Static in C++

--- HelloWorld/src/Main.cpp
+++ HelloWorld/src/Main.cpp
@@ -1,46 +1,11 @@
 #include <iostream>
 
-class Log
-{
-public:
-	const int LogLevelError = 0;
-	const int LogLevelWarning = 1;
-	const int LogLevelInfo = 2;
+extern int s_Variable;
 
-private:
-	int m_LogLevel = LogLevelInfo;
-
-public:
-	void SetLevel(int level)
-	{
-		m_LogLevel = level;
-	}
-
-	void Error(const char* message)
-	{
-		if (m_LogLevel >= LogLevelError)
-			std::cout << "[ERROR]:" << message << std::endl;
-	}
-
-	void Warn(const char* message)
-	{
-		if (m_LogLevel >= LogLevelWarning)
-			std::cout << "[WARNING]:" << message << std::endl;
-	}
-
-	void Info(const char* message)
-	{
-		if (m_LogLevel >= LogLevelInfo)
-			std::cout << "[INFO]:" << message << std::endl;
-	}
-};
+void Function();
 
 int main()
 {
-	Log log;
-	log.SetLevel(log.LogLevelError);
-	log.Warn("Hello!");
-	log.Error("Hello!");
-	log.Info("Hello!");
+	std::cout << s_Variable << std::endl;
 	std::cin.get();
 }
