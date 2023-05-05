ec553c7edf12bb3135b79a143e7fa377dc9b27a4
Maxime Héliot <maxime.heliotpro@gmail.com>
2023-05-05 11:45:06 +0200

    Add Constructors in C++

--- HelloWorld/Log.cpp
+++ HelloWorld/Log.cpp
@@ -38,12 +38,12 @@ public:
 	}
 };
 
-int main()
-{
-	Log log;
-	log.SetLevel(Log::LevelError);
-	log.Warn("Hello!");
-	log.Error("Hello!");
-	log.Info("Hello!");
-	std::cin.get();
-}
+//int main()
+//{
+//	Log log;
+//	log.SetLevel(Log::LevelError);
+//	log.Warn("Hello!");
+//	log.Error("Hello!");
+//	log.Info("Hello!");
+//	std::cin.get();
+//}
--- HelloWorld/src/Main.cpp
+++ HelloWorld/src/Main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 
-enum Example : char
+class Entity
 {
-	A, B, C
+public:
+	float X, Y;
+
+	Entity()
+	{
+		X = 0.0f;
+		Y = 0.0f;
+	}
+
+	Entity(float x, float y)
+	{
+		X = x;
+		Y = y;
+	}
+
+	void Print()
+	{
+		std::cout << X << ", " << Y << std::endl;
+	}
 };
 
+int main()
+{
+	Entity e(10.0f, 5.0f);
+	e.Print();
+	std::cin.get();
+}
